6e2abe6d2a2e5cac31991468816dd3e0351029cf
sfc-gh-jirip <121260139+sfc-gh-jirip@users.noreply.github.com>
2023-01-10 14:23:45 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import streamlit as str
 str.title('My Parents New Healthy Diner')
 
 str.header('Breakfast menu')
-str.text('Omega 3 & Blueberry Cereal')
-str.text('Kale, spinach and rocket smoothie')
-str.text('Hard-boiled egg')
+str.text('🥣 Omega 3 & Blueberry Cereal')
+str.text('🥗 Kale, spinach and rocket smoothie')
+str.text('🐔 Hard-boiled egg')
+str.text('🥑🍞 Avocado toast')
+ 
