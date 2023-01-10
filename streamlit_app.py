2a0db3ce40865741ba74f72d0e0270733e3c6acb
sfc-gh-jirip <121260139+sfc-gh-jirip@users.noreply.github.com>
2023-01-10 14:27:43 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as str
+import pandas as pa
 
 str.title('My Parents New Healthy Diner')
 
@@ -8,3 +9,7 @@ str.text('🥗 Kale, spinach and rocket smoothie')
 str.text('🐔 Hard-boiled egg')
 str.text('🥑🍞 Avocado toast')
 str.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pa.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+str.dataframe(my_fruit_list)
